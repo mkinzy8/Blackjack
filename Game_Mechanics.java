a96a39d794871a2c878187cd7f18f9b7c721330a
Miki K <mkinzy8@students.d125.org>
2016-04-22 10:47:50 -0500

    Initial Classes, empty classes

---
+++ Game_Mechanics.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class Battle_Class here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Game_Mechanics
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Battle_Class
+     */
+    public Game_Mechanics()
+    {
+        
+        
+    }
+
+    public int encntrPrcnt()
+    {
+        
+        return 0;
+    }
+    
+    public int catching()
+    {
+        
+        return 0;
+    }
+    
+    public int criticalHit()
+    {
+        
+        return 0;
+    }
+    
+}
---
+++ Pokemon_Runner.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Battle_Class here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pokemon_Runner
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Battle_Class
+     */
+    public Pokemon_Runner()
+    {
+        
+        
+    }
+
+    
+}
